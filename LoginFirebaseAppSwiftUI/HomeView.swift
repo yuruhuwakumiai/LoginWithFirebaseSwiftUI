fdee7ae94f4a86db2d46f5f70fe02b336a65dc33
yuruhuwakumiai <yuruhuwakumiai@gmail.com>
2023-05-18 05:52:47 +0900

    メールアドレス認証機能

---
+++ LoginFirebaseAppSwiftUI/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  LoginFirebaseAppSwiftUI
+//
+//  Created by 橋元雄太郎 on 2023/05/18.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("ログイン成功！！")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
--- LoginFirebaseAppSwiftUI/LoginFirebaseAppSwiftUIApp.swift
+++ LoginFirebaseAppSwiftUI/LoginFirebaseAppSwiftUIApp.swift
@@ -6,9 +6,23 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+// 追加設定
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+    }
+}
 
 @main
 struct LoginFirebaseAppSwiftUIApp: App {
+    // 追加設定
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
