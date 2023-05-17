6543ac624b7b47de7091630eb9f7f66f151d133a
yuruhuwakumiai <yuruhuwakumiai@gmail.com>
2023-05-18 05:31:27 +0900

    プロジェクト作成

---
+++ LoginFirebaseAppSwiftUI/Firebase.swift
@@ -0,0 +1,8 @@
+//
+//  Firebase.swift
+//  LoginFirebaseAppSwiftUI
+//
+//  Created by 橋元雄太郎 on 2023/05/17.
+//
+
+import Foundation
---
+++ LoginFirebaseAppSwiftUI/LoginFirebaseAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginFirebaseAppSwiftUIApp.swift
+//  LoginFirebaseAppSwiftUI
+//
+//  Created by 橋元雄太郎 on 2023/05/17.
+//
+
+import SwiftUI
+
+@main
+struct LoginFirebaseAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
